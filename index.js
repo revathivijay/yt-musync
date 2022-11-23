a0a6d3c54e6b26b149429e58a5b961bbb6387469
revathivijay <34963337+revathivijay@users.noreply.github.com>
2022-11-23 15:49:40 -0500

    basic working version and controls

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("index"))
+  .get("/home", (req, res) => res.render("index"))
+  .get("/about", (req, res) => res.render("about"))
+  .get("/video", (req, res) => res.render("video"))
+  .get("/video/:uid", function testfn(req, res, next) {
+    // console.log("uid" + req.params.uid);
+    window.location.href = "/home" + "?uid=" + req.params.uid;
+  })
+
+  .get("/home/:uid", function testfn(req, res, next) {
+    window.location.href = "/home" + "?uid=" + req.params.uid;
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
