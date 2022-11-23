c732f468199a0e965a7e1ce67532bf92df02f924
revathivijay <34963337+revathivijay@users.noreply.github.com>
2022-11-23 18:56:34 -0500

    first working version

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const path = require("path");
 var app = express();
 
-var http = require("http");
-var app = express();
-var server = http.createServer(app);
-var io = require("socket.io")(server);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/favicon.ico", express.static("images/favicon.ico"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("index"));
@@ -25,11 +24,17 @@ app.get("/home/:uid", function testfn(req, res, next) {
   window.location.href = "/home" + "?uid=" + req.params.uid;
 });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+http.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("A user connected on socket: " + socket.id);
+
+  socket.on("playEvent", function (msg) {
+    console.log("playEvent: " + JSON.stringify(msg));
+    io.emit("playEvent", msg);
+  });
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("The user disconnected");
   });
 });
